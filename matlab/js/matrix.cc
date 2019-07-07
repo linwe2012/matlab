9496d1243958ecd8642d223858c6de6880a6c21a
Commelina <maosics@gmail.com>
2019-07-08 07:47:41 +0800

    Add some IMAGE process functions, just for test now

--- matlab/js/matrix.cc
+++ matlab/js/matrix.cc
@@ -1,13 +1,25 @@
 #include "shell.h"
-#include "opencv2/core/core.hpp"
+#include "opencv2/opencv.hpp"
 #include <iostream>
 using namespace v8;
 
 class Matrix {
 public:
+	Matrix() {}
+
 	void v8_resize(const v8::FunctionCallbackInfo<v8::Value>& args);
+	void v8_openFile(const v8::FunctionCallbackInfo<v8::Value>& args);
+	void v8_showimg(const v8::FunctionCallbackInfo<v8::Value>& args); // Output pic, to file `output.png`. Just for test.
+	void v8_rotate(const v8::FunctionCallbackInfo<v8::Value>& args);
+	void v8_togray(const v8::FunctionCallbackInfo<v8::Value>& args);
+	void v8_tobin(const v8::FunctionCallbackInfo<v8::Value>& args);
 	
 	void resize(const std::vector<int>& dims);
+	void openFile(const std::string& fileName);
+	void showimg();
+	void rotate(const int degree);
+	void togray();
+	void tobin();
 
 	cv::Mat matrix;
 };
@@ -22,12 +34,23 @@ void ArgError(const v8::FunctionCallbackInfo<v8::Value>& args, char const(&str)[
 
 void DefineJSMatrix(V8Shell* shell) {
 	auto isolate = shell->GetIsolate();
+	v8pp::module mylib(isolate);
 
 	v8pp::class_<Matrix> matrix_class(isolate);
 	matrix_class
-		.set("resize", &Matrix::v8_resize);
+		.ctor<>()
+		.set("resize", &Matrix::v8_resize)
+		.set("matrix", &Matrix::matrix)
+		.set("openFile", &Matrix::openFile)
+		.set("showimg", &Matrix::showimg)
+		.set("rotate", &Matrix::rotate)
+		.set("togray", &Matrix::togray)
+		.set("tobin", &Matrix::tobin);
 
+	mylib.set("Matrix", matrix_class);
 
+	isolate->GetCurrentContext()->Global()->Set(
+		v8::String::NewFromUtf8(isolate, "mylib"), mylib.new_instance());
 	
 }
 
@@ -63,6 +86,52 @@ void Matrix::v8_resize(const v8::FunctionCallbackInfo<v8::Value>& args)
 	resize(dims);
 }
 
+
+void Matrix::v8_openFile(const v8::FunctionCallbackInfo<v8::Value>& args)
+{
+	int num_args = args.Length();
+
+	if (num_args == 0) {
+		return ArgError(args, "Expected at least one argument");
+	}
+
+	std::string fileName = v8pp::from_v8<std::string>(args.GetIsolate(), args[0]);
+	openFile(fileName);
+}
+
+void Matrix::v8_showimg(const v8::FunctionCallbackInfo<v8::Value>& args)
+{
+	int num_args = args.Length();
+
+	if (num_args == 0) {
+		return ArgError(args, "Expected at least one argument");
+	}
+
+	showimg();
+}
+
+void Matrix::v8_rotate(const v8::FunctionCallbackInfo<v8::Value>& args)
+{
+	int num_args = args.Length();
+
+	if (num_args == 0) {
+		return ArgError(args, "Expected at least one argument");
+	}
+
+	int deg = v8pp::from_v8<int>(args.GetIsolate(), args[0]);
+	rotate(deg);
+}
+
+void Matrix::v8_togray(const v8::FunctionCallbackInfo<v8::Value>& args)
+{
+	togray();
+}
+
+void Matrix::v8_tobin(const v8::FunctionCallbackInfo<v8::Value>& args)
+{
+	tobin();
+}
+
 void Matrix::resize(const std::vector<int>& dims)
 {
 	std::cerr << "resize called: [";
@@ -72,3 +141,43 @@ void Matrix::resize(const std::vector<int>& dims)
 	std::cout << "]" << std::endl;
 }
 
+void Matrix::openFile(const std::string& fileName)
+{
+	matrix = cv::imread(fileName);
+	std::cout << fileName << "loaded." << std::endl;
+}
+
+void Matrix::showimg()
+{
+	//std::cout << matrix << std::endl;
+	cv::imwrite("output.png", matrix);
+	//cv::imshow("Pic", matrix);
+}
+
+void Matrix::rotate(const int degree)
+{
+	cv::Mat* temp = new cv::Mat(matrix);
+
+	cv::Point2f center((temp->cols - 1) / 2.0, (temp->rows - 1) / 2.0);
+	cv::Mat rot = cv::getRotationMatrix2D(center, degree, 1.0);
+
+	cv::Rect2f bbox = cv::RotatedRect(cv::Point2f(), temp->size(), degree).boundingRect2f();
+
+	rot.at<double>(0, 2) += bbox.width / 2.0 - temp->cols / 2.0;
+	rot.at<double>(1, 2) += bbox.height / 2.0 - temp->rows / 2.0;
+
+	cv::warpAffine(*temp, matrix, rot, bbox.size());
+}
+
+void Matrix::togray()
+{
+	cv::Mat temp = matrix.clone();
+	cv::cvtColor(temp, matrix, cv::COLOR_BGR2GRAY);
+}
+
+void Matrix::tobin()
+{
+	togray();
+	cv::Mat temp = matrix.clone();
+	cv::threshold(temp, matrix, 15, 100.0, 8);
+}
